65698df2696a3c6554fa5a7874572eee0756913b
gblow <g.blow@epcc.ed.ac.uk>
2022-06-14 11:11:14 +0100

    Update main.tf with first utility and worker node definitions

--- main.tf
+++ main.tf
@@ -37,17 +37,23 @@ data "openstack_images_image_v2" "image" {
 
 ## Get flavor id
 data "openstack_compute_flavor_v2" "jump-flavor" {
-  name = "qserv-jump" # flavor to be used
+  name = "qserv-jump" # flavor to be used for jump
 }
 data "openstack_compute_flavor_v2" "czar-flavor" {
-  name = "qserv-jump" # flavor to be used
+  name = "qserv-jump" # flavor to be used for czar
+}
+data "openstack_compute_flavor_v2" "utility-flavor" {
+  name = "qserv-jump" # flavor to be used for utility nodes
+}
+data "openstack_compute_flavor_v2" "worker-flavor" {
+  name = "qserv-jump" # flavor to be used for worker nodes
 }
 
 resource "openstack_networking_floatingip_v2" "jump" {
  pool = "external" 
 }
 
-# Create an instance
+# Create jump host
 resource "openstack_compute_instance_v2" "jump" {
   name            = "GB-QServ2-Jump"  #Instance name
   image_id        = data.openstack_images_image_v2.image.id
@@ -59,11 +65,35 @@ resource "openstack_compute_instance_v2" "jump" {
     name = var.network
   }
 }
-# Create an instance
+# Create czar
 resource "openstack_compute_instance_v2" "czar" {
   name            = "GB-QServ2-Czar"  #Instance name
   image_id        = data.openstack_images_image_v2.image.id
   flavor_id       = data.openstack_compute_flavor_v2.czar-flavor.id
+  key_pair        = var.keypair
+  security_groups = var.security_groups
+  
+  network {
+    name = var.network
+  }
+}
+
+resource "openstack_compute_instance_v2" "utility" {
+  name            = "GB-QServ2-Utility"  #Instance name
+  image_id        = data.openstack_images_image_v2.image.id
+  flavor_id       = data.openstack_compute_flavor_v2.utility-flavor.id
+  key_pair        = var.keypair
+  security_groups = var.security_groups
+  
+  network {
+    name = var.network
+  }
+}
+
+resource "openstack_compute_instance_v2" "worker" {
+  name            = "GB-QServ2-Worker"  #Instance name
+  image_id        = data.openstack_images_image_v2.image.id
+  flavor_id       = data.openstack_compute_flavor_v2.worker-flavor.id
   key_pair        = var.keypair
   security_groups = var.security_groups
   
