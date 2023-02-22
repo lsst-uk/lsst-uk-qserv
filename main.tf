d324059d42c9125e6f9c0598b13d30f32ae8bc34
gblow <g.blow@epcc.ed.ac.uk>
2023-02-22 13:44:36 +0000

    Update main.tf v3 on attachment

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "openstack_compute_instance_v2" "worker" {
 resource "openstack_compute_volume_attach_v2" "attached" {
   count       = var.worker_count
   instance_id = openstack_compute_instance_v2.worker[count.index].id
-  volume_id   = openstack_blockstorage_volume_v2.worker-vol[count.index].id
+  volume_id   = openstack_blockstorage_volume_v3.worker-vol[count.index].id
 }
 
 resource "openstack_compute_floatingip_associate_v2" "jump" {
