97e6349ddc7e2456c6fa1f7fe3f1d501bcbbb27b
gblow <g.blow@epcc.ed.ac.uk>
2023-02-22 12:37:46 +0000

    Update main.tf second attempt at using count

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "openstack_compute_instance_v2" "worker" {
 
 resource "openstack_compute_volume_attach_v2" "attached" {
   count       = var.worker_count
-  instance_id = "${openstack_compute_instance_v2.worker.*.id}"
-  volume_id   = "${openstack_blockstorage_volume_v2.worker-vol.*.id}"
+  instance_id = "${openstack_compute_instance_v2.worker.count.index.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.worker-vol.count.index.id}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "jump" {
