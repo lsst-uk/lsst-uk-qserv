acf23a5646b4fe99462a44e6b880ba7944c0a066
gblow <g.blow@epcc.ed.ac.uk>
2023-02-22 13:36:45 +0000

    Update main.tf iteration

--- main.tf
+++ main.tf
@@ -127,9 +127,8 @@ resource "openstack_compute_instance_v2" "worker" {
 }
 
 resource "openstack_compute_volume_attach_v2" "attached" {
-  count       = var.worker_count
-  instance_id = openstack_compute_instance_v2.worker[count.index].id
-  volume_id   = openstack_blockstorage_volume_v2.worker-vol[count.index].id
+  instance_id = openstack_compute_instance_v2.worker[1].id
+  volume_id   = openstack_blockstorage_volume_v2.worker-vol[1].id
 }
 
 resource "openstack_compute_floatingip_associate_v2" "jump" {
