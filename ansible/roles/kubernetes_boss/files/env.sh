c038375f8e19b75fefbbfe48d260bea72019fbc2
gblow <g.blow@epcc.ed.ac.uk>
2023-03-07 14:17:25 +0000

    Update env.sh to a temporary version for a new 1 utility 3 worker 1 czar cluster (needs to be dynamically written by terraform template eventually)

--- ansible/roles/kubernetes_boss/files/env.sh
+++ ansible/roles/kubernetes_boss/files/env.sh
@@ -2,6 +2,7 @@
 
 INSTANCE="qserv"
 
+
 # Use current namespace
 NS=$(kubectl config view  --output 'jsonpath={..namespace}')
 NS=$([ ! -z "$NS" ] && echo "$NS" || echo "default")
@@ -9,13 +10,13 @@ NS=$([ ! -z "$NS" ] && echo "$NS" || echo "default")
 PARALLEL_SSH_CFG="$HOME/.ssh/sshqserv"
 PARALLEL_SSH_MASTER="$HOME/.ssh/sshqservmaster"
 
-REPL_DB_HOST="qserv-czar-pool"
-INGEST_DB_HOST="qserv-czar-pool"
+REPL_DB_HOST="gb-qserv2-utility-1"
+INGEST_DB_HOST="gb-qserv2-utility-1"
 
 ROOT_DATA_DIR="/qserv-data"
 DATA_DIR="${ROOT_DATA_DIR}/${NS}"
 
-MASTERS="qserv-czar-pool"
-WORKERS="qserv-utility-pool qserv-worker-pool"
+MASTERS="gb-qserv2-czar"
+WORKERS="gb-qserv2-worker-1 gb-qserv2-worker-2 gb-qserv2-worker-3"
 
 SSH_CFG_OPT="-t"
