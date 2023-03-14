4f831ba94b9094382e414350eba973f34ebcb004
gblow <g.blow@epcc.ed.ac.uk>
2023-03-14 17:14:32 +0000

    Update env.sh

--- ansible/roles/kubernetes_boss/files/env.sh
+++ ansible/roles/kubernetes_boss/files/env.sh
@@ -10,13 +10,13 @@ NS=$([ ! -z "$NS" ] && echo "$NS" || echo "default")
 PARALLEL_SSH_CFG="$HOME/.ssh/sshqserv"
 PARALLEL_SSH_MASTER="$HOME/.ssh/sshqservmaster"
 
-REPL_DB_HOST="gb-qserv2-utility-1"
-INGEST_DB_HOST="gb-qserv2-utility-1"
+REPL_DB_HOST="sv-qserv-utility-1"
+INGEST_DB_HOST="sv-qserv-utility-1"
 
 ROOT_DATA_DIR="/qserv-data"
 DATA_DIR="${ROOT_DATA_DIR}/${NS}"
 
-MASTERS="gb-qserv2-czar"
-WORKERS="gb-qserv2-worker-1 gb-qserv2-worker-2 gb-qserv2-worker-3"
+MASTERS="sv-qserv-czar"
+WORKERS="sv-qserv-worker-1 sv-qserv-worker-2 sv-qserv-worker-3 sv-qserv-worker-4 sv-qserv-worker-5"
 
 SSH_CFG_OPT="-t"
