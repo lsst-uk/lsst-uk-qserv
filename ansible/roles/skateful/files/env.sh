504f46c3a0fba6db9465bcbf7d3317ce7dcde74a
gblow <g.blow@epcc.ed.ac.uk>
2023-10-27 17:33:31 +0100

    Update file env.sh

--- ansible/roles/skateful/files/env.sh
+++ ansible/roles/skateful/files/env.sh
@@ -10,14 +10,14 @@ NS=$([ ! -z "$NS" ] && echo "$NS" || echo "default")
 PARALLEL_SSH_CFG="$HOME/.ssh/sshqserv"
 PARALLEL_SSH_MASTER="$HOME/.ssh/sshqservmaster"
 
-REPL_CTL_HOST="sv-qserv-utility-1"
-REPL_DB_HOST="sv-qserv-utility-1"
-INGEST_DB_HOST="sv-qserv-utility-2"
+REPL_CTL_HOST="sv-qserv-oct23-utility-1"
+REPL_DB_HOST="sv-qserv-oct23-utility-1"
+INGEST_DB_HOST="sv-qserv-oct23-utility-2"
 
 ROOT_DATA_DIR="/qserv-data"
 DATA_DIR="${ROOT_DATA_DIR}/${NS}"
 
-MASTERS="sv-qserv-czar"
-WORKERS="sv-qserv-worker-1 sv-qserv-worker-2 sv-qserv-worker-3 sv-qserv-worker-4 sv-qserv-worker-5 sv-qserv-worker-6 sv-qserv-worker-7 sv-qserv-worker-8 sv-qserv-worker-9 sv-qserv-worker-10"
+MASTERS="sv-qserv-oct23-czar"
+WORKERS="sv-qserv-oct23-worker-1 sv-qserv-oct23-worker-2 sv-qserv-oct23-worker-3 sv-qserv-oct23-worker-4 sv-qserv-oct23-worker-5 sv-qserv-oct23-worker-6 sv-qserv-worker-7 sv-qserv-oct23-worker-8 sv-qserv-oct23-worker-9 sv-qserv-oct23-worker-10"
 
 SSH_CFG_OPT="-t"
